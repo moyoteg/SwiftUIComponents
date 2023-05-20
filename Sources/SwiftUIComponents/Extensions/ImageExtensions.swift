d41d0627423aee29690cf1185612bbe16216e723
Moi Gutierrez <moyoteg@gmail.com>
2023-05-19 20:40:04 -0500

    updated welcome flow

--- Sources/SwiftUIComponents/Extensions/ImageExtensions.swift
+++ Sources/SwiftUIComponents/Extensions/ImageExtensions.swift
@@ -21,3 +21,11 @@ public extension Image {
         }
     }
 }
+
+public extension UIImageView {
+  func setImageColor(color: UIColor) {
+    let templateImage = self.image?.withRenderingMode(.alwaysTemplate)
+    self.image = templateImage
+    self.tintColor = color
+  }
+}
