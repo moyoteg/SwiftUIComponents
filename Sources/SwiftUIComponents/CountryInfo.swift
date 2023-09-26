27308f856323e88a23031e55e1b6cf9415cb2c64
Moi Gutierrez <moyoteg@gmail.com>
2023-09-25 22:20:43 -0600

    updated sign up flow email + text

---
+++ Sources/SwiftUIComponents/CountryInfo.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Moi Gutierrez on 9/20/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
